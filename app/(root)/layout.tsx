50cd2d9f8cf5165a81b64d305378ddc700de1113
afiifatuts <afiifatuts04@gmail.com>
2024-05-04 04:52:08 +0700

    implement complete layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,3 +1,7 @@
+import MobileNav from "@/components/MobileNav";
+import Sidebar from "@/components/Sidebar";
+import Image from "next/image";
+
 
 
 export default function RootLayout({
@@ -5,10 +9,33 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn ={
+    firstName :'Afiifatuts',
+    lastName :'Tsaaniyah',
+    $id: 'string',
+    email: 'string',
+    userId: 'string',
+    dwollaCustomerUrl: 'string',
+    dwollaCustomerId: 'string',
+    name: 'string',
+    address1: 'string',
+    city: 'string',
+    state: 'string',
+    postalCode: 'string',
+    dateOfBirth: 'string',
+    ssn: 'string',
+  }
+
   return (
     <main className="flex h-screen w-full font-inter">
-    Sidebar 
+    <Sidebar user={loggedIn}/> 
     <div className="flex size-full flex-col">
+    <div className="root-layout">
+        <Image src="/icons/logo.svg" width={30} height={30} alt="menu icon"/>
+        <div>
+          <MobileNav user={loggedIn}/>
+        </div>
+      </div>
     {children}
     </div>
   </main>
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,30 +1,47 @@
-import HeaderBox from '@/components/HeaderBox'
-import TotalBalanceBox from '@/components/TotalBalanceBox'
-import React from 'react'
+import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+import React from "react";
 
 const Home = () => {
-    const loggedIn = {firstName:'Afiifatuts'}
+  const loggedIn = {
+    firstName: "Afiifatuts",
+    lastName: "Tsaaniyah",
+    $id: "string",
+    email: "string",
+    userId: "string",
+    dwollaCustomerUrl: "string",
+    dwollaCustomerId: "string",
+    name: "string",
+    address1: "string",
+    city: "string",
+    state: "string",
+    postalCode: "string",
+    dateOfBirth: "string",
+    ssn: "string",
+  };
 
   return (
     <section className="home">
       <div className="home-content">
         <header className="home-header">
-                <HeaderBox
-                type="greeting"
-                title="Welcome"
-                user={loggedIn?.firstName||'Guest'}
-                subtext="Access and menage your account and transactions efficiently."
-                />
-                <TotalBalanceBox
-                accounts={[]}
-                totalBanks={1}
-                totalCurrentBalance={1250}
-
-                />
-            </header>
-        </div>
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and menage your account and transactions efficiently."
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250}
+          />
+        </header>
+        Recent Transaction
+      </div>
+      <RightSidebar user={loggedIn} transactions={[]} banks={[{currentBalance:123.22},{currentBalance:3434.1 }]} />
     </section>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
